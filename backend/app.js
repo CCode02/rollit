2f15d1070f9e2abba504138ae9f35eea01127999
CCode02 <cristiancameloariza@gmail.com>
2026-07-28 17:28:35 -0500

    Creación de middleware para logs de las request y para un endpoint desconocido

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,14 @@
 import express from 'express'
+import middleware from './utils/middleware.js'
 
 const app = express()
+
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+
+
+
+app.use(middleware.unknownEndpoint)
 
 export default app
---
+++ backend/utils/middleware.js
@@ -0,0 +1,18 @@
+import logger from './logger.js'
+
+const requestLogger = (request, response, next) => {
+    logger.info('Method: ', request.method)
+    logger.info('Path: ', request.path)
+    logger.info('Body: ', request.body)
+    logger.info('---')
+    next()
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).json({ error: 'unknown endpoint' })
+}
+
+export default {
+    requestLogger,
+    unknownEndpoint
+}
