ccd2986ddb3b643c1fcaa5cd5fc237e1686d0585
CCode02 <cristiancameloariza@gmail.com>
2026-07-30 16:35:44 -0500

    Conexión a la base de datos añadida

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,21 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import middleware from './utils/middleware.js'
+import logger from './utils/logger.js'
+import config from './utils/config.js'
 
 const app = express()
+
+logger.info('Connecting to', config.MONGODB_URI)
+
+mongoose
+    .connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('Connected to mongoDB')
+    })
+    .catch((error) => {
+        logger.error('Error connection to MongoDB:', error.message)
+    })
 
 app.use(express.json())
 app.use(middleware.requestLogger)
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,7 +1,11 @@
+import dns from 'dns'
 import dotenv from 'dotenv'
+
+dns.setServers(['8.8.8.8', '8.8.4.4']); // Fix for ISP DNS blocking MongoDB SRV lookups
+
 dotenv.config()
 
-
 const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
 
-export default { PORT }
+export default { PORT, MONGODB_URI }
